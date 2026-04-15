e415affae0c19626d3f389ab97125246744060aa
adrienbtr <adrienbuttier@gmail.com>
2026-04-15 14:44:33 +0200

    Sankey

--- viz_data.js
+++ viz_data.js
@@ -457,3 +457,47 @@ const LENS_MONTHLY = {
   ]
 };
 const LENS_MONTH_LABELS = ["Feb 2022", "Mar 2022", "Apr 2022", "May 2022", "Jun 2022", "Jul 2022", "Aug 2022", "Sep 2022", "Oct 2022", "Nov 2022", "Dec 2022", "Jan 2023", "Feb 2023", "Mar 2023", "Apr 2023", "May 2023", "Jun 2023", "Jul 2023", "Aug 2023", "Sep 2023", "Oct 2023", "Nov 2023", "Dec 2023", "Jan 2024", "Feb 2024", "Mar 2024", "Apr 2024", "May 2024", "Jun 2024", "Jul 2024", "Aug 2024", "Sep 2024", "Oct 2024", "Nov 2024", "Dec 2024"];
+
+const SANKEY_DATA = {
+  "nodes": [
+    {
+      "id": "shots",
+      "label": "Picture shots"
+    },
+    {
+      "id": "selection",
+      "label": "Selection"
+    },
+    {
+      "id": "edition",
+      "label": "Edition"
+    },
+    {
+      "id": "published",
+      "label": "Published"
+    }
+  ],
+  "links": [
+    {
+      "source": "shots",
+      "target": "selection",
+      "value": 56882
+    },
+    {
+      "source": "selection",
+      "target": "edition",
+      "value": 56882
+    },
+    {
+      "source": "edition",
+      "target": "published",
+      "value": 500
+    }
+  ],
+  "totals": {
+    "shots": 160574,
+    "selection": 56882,
+    "edition": 56882,
+    "published": 500
+  }
+};
